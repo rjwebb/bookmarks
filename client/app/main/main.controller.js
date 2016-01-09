0fc39feddf5ff720b5a8c76377d7b01d63fcbbf6
Bob Webb <bob.wbb@googlemail.com>
2016-01-09 21:19:54 +0000

    created a form to allow the user to add bookmarks

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,21 +7,30 @@ class MainController {
   constructor($http, $scope, socket) {
     this.$http = $http;
     this.bookmarks = [];
+    this.form_bookmark = {};
 
     $scope.lol = 10013013;
 
     $http.get('/api/bookmarks').then(response => {
       this.bookmarks = response.data;
-      console.log(this.bookmarks);
       socket.syncUpdates('bookmark', this.bookmarks);
     });
 
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('bookmark');
     });
-
   }
 
+  addBookmark() {
+    if(this.form_bookmark.name && this.form_bookmark.url){
+      this.$http.post('/api/bookmarks', {
+	name: this.form_bookmark.name,
+	url: this.form_bookmark.url
+      });
+
+      this.form_bookmark = {};
+    }
+  }
 }
 
 angular.module('bookmarksApp')
