35c8d148491a2e877db77671437c3db6ab1ec2ad
Bob Webb <bob.wbb@googlemail.com>
2016-01-12 13:44:39 +0000

    added tests for bookmarkform

--- client/app/bookmarkform/bookmarkform.service.spec.js
+++ client/app/bookmarkform/bookmarkform.service.spec.js
@@ -6,13 +6,44 @@ describe('Service: bookmarkform', function () {
   beforeEach(module('bookmarksApp'));
 
   // instantiate service
-  var bookmarkform;
-  beforeEach(inject(function (_bookmarkform_) {
-    bookmarkform = _bookmarkform_;
+  var bookmarkform, $httpBackend;
+  beforeEach(inject(function ($injector) {
+    bookmarkform = $injector.get('bookmarkform');
+    bookmarkform.fields = {};
+    $httpBackend = $injector.get('$httpBackend');
   }));
 
-  it('should do something', function () {
-    expect(!!bookmarkform).toBe(true);
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   });
+
+
+  describe('Method: addBookmark', function () {
+    beforeEach(function (){
+      $httpBackend.expectGET('app/main/main.html').respond(200, '');
+    }
+
+    it('addBookmark should not add a bookmark if name and url are missing', function() {
+      bookmarkform.addBookmark();
+    });
+
+    it('addBookmark should not add a bookmark if only url is given', function() {
+      bookmarkform.fields.url = "http://www.facebook.com";
+      bookmarkform.addBookmark();
+    });
+
+    it('addBookmark should not add a bookmark if only name is given', function() {
+      bookmarkform.fields.name = "Facebook";
+      bookmarkform.addBookmark();
+    });
+
+    it('addBookmark should add a bookmark if name and url are given', function() {
+      bookmarkform.fields.url = "http://www.facebook.com";
+      bookmarkform.fields.name = "Facebook";
+      bookmarkform.addBookmark();
+      $httpBackend.expectPOST('/api/bookmarks').respond(200, '');
+    });
+  }
 
 });
