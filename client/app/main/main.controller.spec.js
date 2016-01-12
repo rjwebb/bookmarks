e6f9a9377d7f620a992403ddcbdc127d520dfc3e
Bob Webb <bob.wbb@googlemail.com>
2016-01-12 13:44:09 +0000

    refactored main test file

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -1,21 +1,30 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: MainController', function () {
 
   // load the controller's module
   beforeEach(module('bookmarksApp'));
 
-  var MainCtrl, scope;
+  var createController, $controller, $httpBackend, $rootScope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainController', {
-      $scope: scope
-    });
+  beforeEach(inject(function ($injector) {
+    $httpBackend = $injector.get('$httpBackend');
+    $rootScope = $injector.get('$rootScope');
+    $controller = $injector.get('$controller');
+
+    $httpBackend.when('GET', '/api/bookmarks')
+      .respond( { data: [] });
+
+    createController = function() {
+      return $controller('MainController', { $scope: $rootScope });
+    };
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  it('should request the bookmarks when the page is loaded', function () {
+    var controller = createController();
+    console.log($rootScope);
+    $httpBackend.flush();
   });
+
 });
